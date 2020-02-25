14d9fd06e70a73294549fd7236b3c9be7edd463a
Hna <ghnruf@naver.com>
2020-02-25 14:55:56 +0900

    Add | ft_memcmp.c

---
+++ dir_test/test_memcmp.c
@@ -0,0 +1,25 @@
+#include "ft_test.h"
+#include "ft.h"
+
+int		main(int argc, char **argv)
+{
+	int		size;
+	char	buf[22] = "abcdefghijklmnopqr";
+	char	buf2[22] = "abcdefghijklmnopqr";
+	int		ret;
+
+	if (argc == 1)
+		return (0);
+
+	size = atoi(argv[1]);
+	strcpy(buf, "0123456aaaaa");
+	printf("%s\n", buf);
+	strcpy(buf2, "0123456bbbbb");
+	printf("%s\n", buf2);
+	ret = memcmp(buf, buf2, size);
+	printf("memcmp(buf, buf2, %d)\n%d\n", size, ret);
+	ret = ft_memcmp(buf, buf2, size);
+	printf("ft_memcmp(buf, buf2, %d)\n%d\n", size, ret);
+
+	return (0);
+}
--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 19:18:11 by hna               #+#    #+#             */
-/*   Updated: 2020/02/25 12:04:10 by hna              ###   ########.fr       */
+/*   Updated: 2020/02/25 14:49:10 by hna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *src, int c, size_t len);
+int		ft_memcmp(const void *s1, const void *s2, size_t len);
 int		ft_strlen(char *str);
 int		ft_isalpha(int c);
 
