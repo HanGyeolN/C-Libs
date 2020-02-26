cab73a81b4ba9561e63c72de9f391349c23627d0
Hna <ghnruf@naver.com>
2020-02-27 00:26:27 +0900

    Add | ft_list function

--- dir_test/test_list.c
+++ dir_test/test_list.c
@@ -1,15 +1,32 @@
 #include "ft.h"
 #include "ft_test.h"
 
+void	del(void *content)
+{
+	free(content);
+}
+
+void	test(void *str)
+{
+	int		i;
+
+	i = 0;
+	*(char *)str = 'A';
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	**list;
 	t_list	*n1;
 	t_list	*n2;
 	t_list	*n3;
+	t_list	*last;
+	t_list	*n4;
+	char	*test_str;
+	int		len;
 
 	if (argc || argv)
-		printf("./test_lstnew <n1> <n2> <n3>\n");
+		printf("./test_lstnew <n1> <n2> <n3> <n4>\n");
 	n1 = ft_lstnew((void *)argv[1]);
 	list = &n1;
 
@@ -19,10 +36,35 @@ int	main(int argc, char **argv)
 	n3 = ft_lstnew((void *)argv[3]);
 	ft_lstadd_front(list, n3);
 
+	test_str = malloc(10);
+	strcpy(test_str, "hello war");
+
+	n4 = ft_lstnew((void *)test_str);	
+	ft_lstadd_back(list, n4);
+
+	last = ft_lstlast(n1);
+
 	while (*list)
 	{
-		printf("%s\n", (char *)(*list)->content);
+		printf("%s -> ", (char *)(*list)->content);
 		*list = (*list)->next;
 	}
+	printf("%s\n", (char *)(*list));
+
+	len = ft_lstsize(n3);
+	printf("ft_lstsize: %d\n", len);
+
+	printf("ft_lstlast: %s\n", (char *)last->content);
+
+	ft_lstiter(n3, &test);
+//	ft_lstclear(list, &del);
+	list = &n3;
+	while (*list)
+	{
+		printf("%s -> ", (char *)(*list)->content);
+		*list = (*list)->next;
+	}
+	printf("%s\n", (char *)(*list));
+
 	return (0);
 }
--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 19:18:11 by hna               #+#    #+#             */
-/*   Updated: 2020/02/26 19:05:10 by hna              ###   ########.fr       */
+/*   Updated: 2020/02/27 00:13:34 by hna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,11 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
