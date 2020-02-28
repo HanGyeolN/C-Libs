e36fded9c8b9810fea9f7170748a38b4257b4235
Hna <ghnruf@naver.com>
2020-02-29 05:25:06 +0900

    Fix | ft_strncmp.c

--- srcs/part1/ft_strncmp.c
+++ srcs/part1/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/25 18:33:24 by hna               #+#    #+#             */
-/*   Updated: 2020/02/25 19:41:31 by hna              ###   ########.fr       */
+/*   Updated: 2020/02/29 05:24:52 by hna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t		i;
+	size_t			i;
+	unsigned char	c1;
+	unsigned char	c2;
 
 	if (n == 0)
 		return (0);
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] != s2[i] || s1[i] == '\0' || s2[i] == '\0')
-			return (s1[i] - s2[i]);
+		c1 = s1[i];
+		c2 = s2[i];
+		if (c1 != c2 || c1 == '\0' || c2 == '\0')
+			return (c1 - c2);
 		i++;
 	}
 	i--;
-	return (s1[i] - s2[i]);
+	return (c1 - c2);
 }
