f1088f231eb4eb45820a4733c4f6ebd1887e2ade
Hna <ghnruf@naver.com>
2020-03-05 13:14:14 +0900

    Add | ft_strrev.c

--- srcs/part3/ft_strrev.c
+++ srcs/part3/ft_strrev.c
@@ -5,23 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hna <hna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/05 13:11:05 by hna               #+#    #+#             */
-/*   Updated: 2020/03/05 13:11:22 by hna              ###   ########.fr       */
+/*   Created: 2020/03/05 13:12:30 by hna               #+#    #+#             */
+/*   Updated: 2020/03/05 13:13:48 by hna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
 
-char			*ft_revstr(char *str)
+char			*ft_strrev(char *str)
 {
 	int		i;
 	int		len;
+	char	temp;
 
 	i = 0;
 	len = ft_strlen(str);
 	while (i < (len / 2))
 	{
-		ft_swap_char(&str[i], &str[len - i - 1]);
+		temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
 		i++;
 	}
 	return (str);
